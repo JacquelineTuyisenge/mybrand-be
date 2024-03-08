484e0aed387fe0b43c9c9208dc67fe924213a040
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-03-08 17:50:16 +0200

    making POST CRUD

---
+++ learn-express/routes.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const Post = require("./models/post");
+const router = express.Router();
+
+router.get("/posts", async (req, res) => {
+  const posts = await Post.find();
+  res.send(posts);
+});
+
+router.post("/posts", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  await post.save();
+  res.send(post);
+});
+
+// individual blog post
+router.get("/posts/:id", async (req, res) => {
+  try {
+    const post = await Post.findOne({ _id: req.params.id });
+    res.send(post);
+  } catch {
+    res.status(404);
+    res.send({ error: "Post doesn't exist!" });
+  }
+});
+
+// incase id passed in is not correct
+router.patch("/posts/:id", async (req, res) => {
+  try {
+    const post = await Post.findOne({ _id: req.params.id });
+
+    if (req.body.title) {
+      post.title = req.body.title;
+    }
+
+    if (req.body.content) {
+      post.content = req.body.content;
+    }
+
+    await post.save();
+    res.send(post);
+  } catch {
+    res.status(404);
+    res.send({ error: "Post doesn't exist!" });
+  }
+});
+
+// delete blog post
+router.delete("/posts/:id", async (req, res) => {
+  try {
+    await Post.deleteOne({ _id: req.params.id });
+    res.status(204).send();
+  } catch {
+    res.status(404);
+    res.send({ error: "Post doesn't exist!" });
+  }
+});
+
+module.exports = router;
